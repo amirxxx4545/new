3e4536ce5f10f5573acf0279b8b79426912f44bc
Amir <khakia799@gmail.com>
2024-06-12 03:49:51 +0330

    mannwar

--- new.py
+++ new.py
@@ -1,1 +1,17 @@
-print('1233')
+print('Amirrr')
+
+
+def s(n1,n2):
+    return n1+n2
+
+
+
+print(s(12,4))
+
+
+for i in range(1,10):
+    print()
+    for j in range(1,10):
+        print(f'{i*j}',end="  ")
+            
+         
